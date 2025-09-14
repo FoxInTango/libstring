352e22608f67b6b0c5b3c345d3ed3af219cc811f
foxintango@allinone.llama <foxintango@allinone.llama>
2025-09-15 07:35:31 +0800

    Mon Sep 15 07:35:31 AM CST 2025

--- .make/depend.dir/libcpp.mk
+++ .make/depend.dir/libcpp.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libcpp.recursive
 ECHO_TARGETS += libcpp.echo
 libcpp.recursive:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libstring/               >> /home/lidali/alpine/libraries/libcpp/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libstring/.make >> /home/lidali/alpine/libraries/libcpp/.make/super
-	cd /home/lidali/alpine/libraries/libcpp/ && make recursive && make install
-	-rm /home/lidali/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libstring/               >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libstring/.make >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
+	cd /volumes/llama/home/alpine/libraries/libcpp/ && make recursive && make install
+	-rm /volumes/llama/home/alpine/libraries/libcpp/.make/super
 libcpp.echo:
-	@echo SUPER_MAKE_DIR=/home/lidali/alpine/libraries/libstring/               >> /home/lidali/alpine/libraries/libcpp/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/home/lidali/alpine/libraries/libstring/.make >> /home/lidali/alpine/libraries/libcpp/.make/super
-	cd /home/lidali/alpine/libraries/libcpp/ && make echo
-	-rm /home/lidali/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libstring/               >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libstring/.make >> /volumes/llama/home/alpine/libraries/libcpp/.make/super
+	cd /volumes/llama/home/alpine/libraries/libcpp/ && make echo
+	-rm /volumes/llama/home/alpine/libraries/libcpp/.make/super
